c90bee5af3444b03cd2388ef7fd7d8b859e35770
v-v-d <v-v-d@bk.ru>
2020-06-14 19:02:53 +0300

    script commented

--- data_analyser.py
+++ data_analyser.py
@@ -34,85 +34,92 @@ def load_df_to_excel(output_filepath, df_resolver):
             df.to_excel(writer, sheet_name=sheet_name, index=False)
 
 
-persons_small_fpath = os.path.join('source', 'small_data_persons.json')
-persons_big_fpath = os.path.join('source', 'big_data_persons.json')
-# contacts_small_fpath = os.path.join('source', 'small_data_contracts.json')
-# contacts_big_fpath = os.path.join('source', 'big_data_contracts.json')
+if __name__ == '__main__':
+    persons_small_fpath = os.path.join('source', 'small_data_persons.json')
+    persons_big_fpath = os.path.join('source', 'big_data_persons.json')
 
-df_resolver = get_df_resolver(
-    persons_small_fpath, persons_big_fpath,
-    # contacts_small_fpath, contacts_big_fpath
-)
-
-# Split small_data_persons column 'Name' and update df_resolver
-small_data_df = df_resolver['small_data_persons']
-small_data_df_split = get_df_with_split_column(
-    small_data_df, 'Name', 'LastName', 'FirstName'
-)
-df_resolver.update({'small_data_persons': small_data_df_split})
-
-# Split big_data_persons column 'Name' and update df_resolver
-big_data_df = df_resolver['big_data_persons']
-big_data_df_split = get_df_with_split_column(
-    big_data_df, 'Name', 'LastName', 'FirstName'
-)
-df_resolver.update({'big_data_persons': big_data_df_split})
-
-df_resolver['small_data_persons'].sort_values(by=['LastName'], inplace=True)
-df_resolver['big_data_persons'].sort_values(by=['FirstName'], inplace=True)
-
-unique_data_persons_by_last_name_df = (
-    df_resolver['small_data_persons'].merge(
-        df_resolver['big_data_persons'], how='outer', indicator=True,
-        on=['LastName'], suffixes=['', '_']
-        # on=['Age', 'FirstName', 'LastName'], suffixes=['', '_']
+    # Make dataframe resolver with key as sheet name and value as dataframe
+    df_resolver = get_df_resolver(
+        persons_small_fpath, persons_big_fpath,
     )
-    .query("_merge=='left_only'")
-    .dropna(how='all', axis=1)
-    .drop(['_merge'], axis=1)
-)
 
-unique_data_persons_df = (
-    df_resolver['small_data_persons'].merge(
-        df_resolver['big_data_persons'], how='outer', indicator=True,
-        on=['Age', 'FirstName', 'LastName'], suffixes=['', '_']
+    # Split small_data_persons and big_data_persons column 'Name'
+    small_data_df = df_resolver['small_data_persons']
+    small_data_df_split = get_df_with_split_column(
+        small_data_df, 'Name', 'LastName', 'FirstName'
     )
-    .query("_merge=='left_only'")
-    .dropna(how='all', axis=1)
-    .drop(['_merge'], axis=1)
-)
+    df_resolver.update({'small_data_persons': small_data_df_split})
 
-df_resolver.update({
-    'unique_persons_by_last_name': unique_data_persons_by_last_name_df,
-    'unique_persons': unique_data_persons_df,
-})
+    big_data_df = df_resolver['big_data_persons']
+    big_data_df_split = get_df_with_split_column(
+        big_data_df, 'Name', 'LastName', 'FirstName'
+    )
+    df_resolver.update({'big_data_persons': big_data_df_split})
 
-all_persons_df = pd.concat([
-    df_resolver['big_data_persons'], df_resolver['unique_persons']
-])
+    # Sort data by Name
+    df_resolver['small_data_persons'].sort_values(
+        by=['LastName'], inplace=True
+    )
+    df_resolver['big_data_persons'].sort_values(
+        by=['FirstName'], inplace=True
+    )
 
-age_mask = all_persons_df.groupby(['LastName'])['Age'].transform(
-    lambda ages: [any(abs(age - ages) == 10) for age in ages]
-)
-namesakes_df = all_persons_df.loc[age_mask].sort_values(by=['LastName'])
+    # Find unique persons in the small_data_persons only by LastName
+    unique_data_persons_by_last_name_df = (
+        df_resolver['small_data_persons'].merge(
+            df_resolver['big_data_persons'], how='outer', indicator=True,
+            on=['LastName'], suffixes=['', '_']
+        )
+        .query("_merge=='left_only'")
+        .dropna(how='all', axis=1)
+        .drop(['_merge'], axis=1)
+    )
 
-df_resolver.update({
-    'namesakes_with_age_diff': namesakes_df,
-})
+    # Find absolutely unique persons in the small_data_persons
+    unique_data_persons_df = (
+        df_resolver['small_data_persons'].merge(
+            df_resolver['big_data_persons'], how='outer', indicator=True,
+            on=['Age', 'FirstName', 'LastName'], suffixes=['', '_']
+        )
+        .query("_merge=='left_only'")
+        .dropna(how='all', axis=1)
+        .drop(['_merge'], axis=1)
+    )
 
-english_chars_mask = (
-    all_persons_df['LastName'].str.contains('[a-zA-Z]', regex=True) |
-    all_persons_df['FirstName'].str.contains('[a-zA-Z]', regex=True)
-)
-persons_with_bad_names_df = all_persons_df.loc[english_chars_mask]
+    df_resolver.update({
+        'unique_persons_by_last_name': unique_data_persons_by_last_name_df,
+        'unique_persons': unique_data_persons_df,
+    })
 
-df_resolver.update({
-    'persons_with_bad_names': persons_with_bad_names_df,
-})
+    # Make all persons dataframe
+    all_persons_df = pd.concat([
+        df_resolver['big_data_persons'], df_resolver['unique_persons']
+    ])
 
-# load data to excel
-if not os.path.exists('result'):
-    os.makedirs('result')
+    # Find namesakes by age rule
+    age_mask = all_persons_df.groupby(['LastName'])['Age'].transform(
+        lambda ages: [any(abs(age - ages) == 10) for age in ages]
+    )
+    namesakes_df = all_persons_df.loc[age_mask].sort_values(by=['LastName'])
 
-output_filepath = os.path.join('result', 'data_persons.xlsx')
-load_df_to_excel(output_filepath, df_resolver)
+    df_resolver.update({
+        'namesakes_with_age_diff': namesakes_df,
+    })
+
+    # Find persons with bad names
+    english_chars_mask = (
+        all_persons_df['LastName'].str.contains('[a-zA-Z]', regex=True) |
+        all_persons_df['FirstName'].str.contains('[a-zA-Z]', regex=True)
+    )
+    persons_with_bad_names_df = all_persons_df.loc[english_chars_mask]
+
+    df_resolver.update({
+        'persons_with_bad_names': persons_with_bad_names_df,
+    })
+
+    # load data to excel
+    if not os.path.exists('result'):
+        os.makedirs('result')
+
+    output_filepath = os.path.join('result', 'data_persons.xlsx')
+    load_df_to_excel(output_filepath, df_resolver)
